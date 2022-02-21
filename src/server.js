b75cabc5c51f171484fd66f145dcc27e9de5922b
Allie Maus <aem3780@rit.edu>
2022-02-21 10:49:43 -0500

    more progress on prototype

---
+++ src/server.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const htmlHandler = require('./htmlResponses');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const onRequest = (request, response) => {
+  console.log(request.url);
+
+  switch (request.url) {
+    case '/':
+      htmlHandler.getIndex(request, response);
+      break;
+    case '/style.css':
+      htmlHandler.getCSS(request, response);
+      break;
+    default:
+      htmlHandler.getIndex(request, response);
+      break;
+  }
+};
+http.createServer(onRequest).listen(port, () => {
+  console.log(`Listening on 127.0.0.1:${port}`);
+});
