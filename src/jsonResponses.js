8d9909d63c0eba5e22b42e66703768800a02693b
Allie Maus <aem3780@rit.edu>
2022-02-26 15:20:29 -0500

    project progress

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -54,9 +54,46 @@ const notFoundMeta = (request, response) => {
   return respondJSONMeta(request, response, responseCode);
 };
 
+const addUser = (request, response, body) => {
+  responseCode = 400;
+  const responseJSON = {
+    message: 'Username and Title are both required.',
+  };
+
+  if (!body.title || !body.username) {
+    responseJSON.id = 'addMissingParams';
+    return respondJSON(request, response, responseCode, responseJSON);
+  }
+
+  responseCode = 204;
+  if (!users[body.username]) {
+    responseCode = 201;
+    users[body.username] = {};
+    users[body.username].username = body.username;
+    users[body.username].booklist = [];
+  }
+
+  const book = {
+    title: users[body.username].title,
+    author: users[body.username].author,
+    genre: users[body.username].genre,
+    pages: users[body.username].pages,
+  };
+
+  users[body.username].booklist.push(book);
+  console.log(book);
+
+  if (responseCode === 201) {
+    responseJSON.message = 'Created Successfully.';
+    return respondJSON(request, response, responseCode, responseJSON);
+  }
+  return respondJSONMeta(request, response, responseCode);
+};
+
 module.exports = {
   getUsers,
   getUsersMeta,
+  addUser,
   notFound,
   notFoundMeta,
 };
